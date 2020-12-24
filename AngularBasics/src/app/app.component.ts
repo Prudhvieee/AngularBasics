f705f3b362ac4c251e9e63cd1232e1d6d4c897b0
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-24 22:55:03 +0530

    [Prudhvi] add . launching website using event binding

--- AngularBasics/src/app/app.component.ts
+++ AngularBasics/src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularBasics';
   imgUrl: string;
+  url:string;
   ngOnInit():void {
     this.title="Hello from Bridgelabz";
     this.imgUrl='../assets/Bridgelabz.png';
+    this.url="https://www.bridgelabz.com/";
+  }
+   onClick($event){
+    console.log("mouse clicked", $event);
+    window.open(this.url);
    }
 }
